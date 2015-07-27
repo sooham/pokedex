823b6101225efd26cf7a25ab1f06a90f4ccd36cd
sooham <rafizsooham@gmail.com>
2015-07-27 16:58:12 -0400

    trying self made xml

--- request_handler/views.py
+++ request_handler/views.py
@@ -113,10 +113,12 @@ def pokemon_no(request, num):
             [name, str(national_id), types, ability, weight]
         )
 
-        response = twilio.twiml.Response()
-        response.message(sprite)
-        response.message(pokemon_info)
-        return HttpResponse(response.toxml(), content_type='text/xml')
+        response = "<Response><Message><Media>" + sprite + "</Media><Body>" + pokemon_info + "</Body></Message></Response>"
+        return HttpResponse(response, content_type='text/xml')
+        # response = twilio.twiml.Response()
+        # response.message(sprite)
+        # response.message(pokemon_info)
+        # return HttpResponse(response.toxml(), content_type='text/xml')
 
 
 def about(request):
